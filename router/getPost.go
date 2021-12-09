d1943d8f847adcc02ac062e5ec5c63a806db13b1
koldin108902 <alex108902@naver.com>
2021-12-09 18:29:17 +0900

    디버깅, 에러 핸들링

--- router/getPost.go
+++ router/getPost.go
@@ -20,27 +20,35 @@ func GetPost(res http.ResponseWriter, req *http.Request) {
 	page -= 1 //each page's count
 	eachPostConunt := 30
 
+	//만약 queryString에 page값이 없으면 page를 0으로 셋
 	if err != nil {
-		panic(err.Error())
+		page = 0
 	}
 
-	post, err := db.Query("SELECT * FROM post LIMIT ? ?", page, eachPostConunt)
+	query := "SELECT * FROM post LIMIT " + strconv.Itoa(page) + ", " + strconv.Itoa(eachPostConunt) + ";"
+	post, err := db.Query(query)
 
 	var posts []structure.DB
 
 	if err != nil {
+		fmt.Print(err)
 		res.WriteHeader(500)
 		fmt.Fprint(res, "error during get from db")
+		return
 	}
 
 	//append dataes to posts
 	for post.Next() {
 		var row structure.DB
-		post.Scan(&row.Id, &row.Title, &row.Description, &row.Good,
+		err := post.Scan(&row.Id, &row.Title, &row.Description, &row.Good,
 			&row.Bad, &row.ReplyCount, &row.View, &row.Created)
+		if err != nil {
+			panic(err.Error())
+		}
 		posts = append(posts, row)
 	}
 
+	//value가 없으면 not found return
 	if len(posts) < 1 {
 		res.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(res, "there are no posts")
