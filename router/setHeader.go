2399eda83c3778ec31501f63301855420dd4cf5e
koldin108902 <alex108902@naver.com>
2021-12-12 19:56:10 +0900

    credentials옵션 추가

--- router/setHeader.go
+++ router/setHeader.go
@@ -15,7 +15,8 @@ func SetHeader(next http.Handler) http.Handler {
 		}
 		headres := strings.Join(strkeys, ",") + ", X-Naver-Client-Id,X-Naver-Client-Secret,X-TARGET-URL,Content-Type"
 
-		res.Header().Set("Access-Control-Allow-Origin", "*")
+		res.Header().Set("Access-Control-Allow-Origin", "http://localhost:5000")
+		res.Header().Set("Access-Control-Allow-Credentials", "true")
 		res.Header().Set("Access-Control-Allow-Methods", "*")
 		res.Header().Set("Content-Type", "application/json") //res type json set
 		res.Header().Set("Access-Control-Allow-Headers", headres)
