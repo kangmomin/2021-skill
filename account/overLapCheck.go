f5f9bb573db5e8915dd1528c74e54c92da4ac281
koldin108902 <alex108902@naver.com>
2021-12-12 19:24:44 +0900

    res방식 json으로 통합

--- account/overLapCheck.go
+++ account/overLapCheck.go
@@ -16,7 +16,12 @@ type StuendtId struct {
 	StuendtId string
 }
 
+type response struct {
+	IsOverlap bool
+}
+
 func IdOverLap(res http.ResponseWriter, req *http.Request) {
+	var resVal response
 	var body Id
 	var overLapId int
 	err := json.NewDecoder(req.Body).Decode(&body)
@@ -28,20 +33,27 @@ func IdOverLap(res http.ResponseWriter, req *http.Request) {
 
 	err = db.QueryRow("SELECT id FROM account WHERE accountId=?", body.Id).Scan(&overLapId)
 
-	if errString := err.Error(); err != nil && errString != "sql: no rows in result set" { //가져온 값이 없더라도 넘어감.
-		panic(err)
+	if err != nil { //가져온 값이 없더라도 넘어감.
+		if errString := err.Error(); errString != "sql: no rows in result set" {
+			panic(err)
+		}
 	}
 
 	res.WriteHeader(200)
 	if overLapId == 0 { //overlap값이 없을때 false를 리턴
-		fmt.Fprint(res, false)
+		resVal.IsOverlap = false
+		resJSON, _ := json.Marshal(resVal)
+		fmt.Fprint(res, string(resJSON))
 		return
 	}
-	fmt.Fprint(res, true)
+	resVal.IsOverlap = true
+	resJSON, _ := json.Marshal(resVal)
+	fmt.Fprint(res, string(resJSON))
 }
 
 //사실상 같은 코드이지만 생각하기가 귀찮았음...
 func StuendtIdOverLap(res http.ResponseWriter, req *http.Request) {
+	var resVal response
 	var body StuendtId
 	var overLapStuendtId int
 	err := json.NewDecoder(req.Body).Decode(&body)
@@ -49,19 +61,26 @@ func StuendtIdOverLap(res http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+
 	db := conn.DB
 
 	//db에서 입력된 학번과 일치하는 값이 있는지 확인
-	err = db.QueryRow("SELECT id FROM account WHERE stuendtId=?", body.StuendtId).Scan(&overLapStuendtId)
+	err = db.QueryRow("SELECT id FROM account WHERE studentId=?", body.StuendtId).Scan(&overLapStuendtId)
 
-	if errString := err.Error(); err != nil && errString != "sql: no rows in result set" { //가져온 값이 없더라도 넘어감.
-		panic(err)
+	if err != nil { //가져온 값이 없더라도 넘어감.
+		if errString := err.Error(); errString != "sql: no rows in result set" {
+			panic(err)
+		}
 	}
 
 	res.WriteHeader(200)
 	if overLapStuendtId == 0 { //overlap값이 없을때 false를 리턴
-		fmt.Fprint(res, false)
+		resVal.IsOverlap = false
+		resJSON, _ := json.Marshal(resVal)
+		fmt.Fprint(res, string(resJSON))
 		return
 	}
-	fmt.Fprint(res, true)
+	resVal.IsOverlap = true
+	resJSON, _ := json.Marshal(resVal)
+	fmt.Fprint(res, string(resJSON))
 }
