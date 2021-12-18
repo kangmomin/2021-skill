c031c614003441f5f4adeb25ec4bebba2f150230
koldin108902 <alex108902@naver.com>
2021-12-19 00:39:36 +0900

    decode tocken 리팩토링

--- router/getAuthImg.go
+++ router/getAuthImg.go
@@ -1,17 +1,13 @@
 package router
 
 import (
+	"2021skill/account"
 	"2021skill/conn"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
-	"strconv"
-
-	"github.com/lemon-mint/vbox"
 )
 
 type body struct {
@@ -24,23 +20,18 @@ func GetAuthImg(res http.ResponseWriter, req *http.Request) {
 
 	json.NewDecoder(req.Body).Decode(&body)
 
-	reader, _ := os.Open("config/accessKey.txt")
-	key, _ := ioutil.ReadAll(reader)
-	auth := vbox.NewBlackBox(key)
+	userId, _err := account.DecodeTocken(body.Auth)
 
-	userIdByte, _ := auth.Open([]byte(body.Auth))
-	userId, err := strconv.Atoi(hex.EncodeToString(userIdByte))
-
-	if err != nil {
-		fmt.Printf("error getAuthImg.go - 34 \n %s \n", err)
-		fmt.Fprint(res, "error during decode auth key")
+	if _err {
+		res.WriteHeader(400)
+		fmt.Fprint(res, "error during decode tocken")
 		return
 	}
 
 	db := conn.DB
 	var userAuthImg string
 
-	err = db.QueryRow("SELECT authImg FROM account WHERE id=?", userId).Scan(&userAuthImg)
+	err := db.QueryRow("SELECT authImg FROM account WHERE id=?", userId).Scan(&userAuthImg)
 
 	if err != nil {
 		fmt.Printf("error getAuthImg.go - 45 \n %s \n", err)
