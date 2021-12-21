bfa137225482dfafca6e7545462fd3b86abd4dbc
koldin108902 <alex108902@naver.com>
2021-12-21 22:08:40 +0900

    함수 통일화

--- router/image.go
+++ router/image.go
@@ -24,15 +24,13 @@ type resValue struct {
 	Err     bool   `json:"error"`
 }
 
-func UploadImg(res http.ResponseWriter, req *http.Request) {
+func ImageUploader(res http.ResponseWriter, req *http.Request) {
 	//폼 파일 포맷의 image를 가져옴
 	//header에는 img의 정보 img는 그냥 img만
 	img, imgHeader, err := req.FormFile("image")
 	var resValue resValue
 
 	if err != nil {
-		fmt.Println("err in uploadImg.go 25")
-
 		resValue.Err = true
 		resValue.Message = "error during get image data"
 
@@ -51,7 +49,13 @@ func UploadImg(res http.ResponseWriter, req *http.Request) {
 	imgName = strings.ReplaceAll(imgName, ":", "") //img이름 설정시 생기는 공백 제거
 
 	//파일 업로드
-	err = ioutil.WriteFile("public/"+imgName, encodedImg, 0644)
+	fileType := "public/"
+
+	if req.URL.Path == "/auth-image" {
+		fileType = "auth"
+	}
+
+	err = ioutil.WriteFile(fileType+imgName, encodedImg, 0644)
 
 	if err != nil {
 		fmt.Println(err)
@@ -106,52 +110,4 @@ func GetAuthImg(res http.ResponseWriter, req *http.Request) {
 
 	res.Header().Set("Content-Type", "image/jpeg") // <-- set the content-type header
 	io.Copy(res, img)
-}
-
-func UploadAuthImg(res http.ResponseWriter, req *http.Request) {
-	//폼 파일 포맷의 image를 가져옴
-	//header에는 img의 정보 img는 그냥 img만
-	img, imgHeader, err := req.FormFile("image")
-	var resValue resValue
-
-	if err != nil {
-		fmt.Println("err in uploadImg.go 25")
-
-		resValue.Err = true
-		resValue.Message = "error during get image data"
-
-		fmt.Println(err)
-		res.WriteHeader(400)
-		fmt.Fprint(res, "error during get image data")
-		return
-	}
-
-	//이미지 인코딩
-	encodedImg, _ := ioutil.ReadAll(img)
-
-	//이미지가 들어갈 위치와 이름 설정
-	imgName := time.Now().String() + imgHeader.Filename
-	imgName = strings.ReplaceAll(imgName, " ", "") //img이름 설정시 생기는 공백 제거
-	imgName = strings.ReplaceAll(imgName, ":", "") //img이름 설정시 생기는 공백 제거
-
-	//파일 업로드
-	err = ioutil.WriteFile("authImg/"+imgName, encodedImg, 0644)
-
-	if err != nil {
-		fmt.Println(err)
-		resValue.Message = "cannot uplaod the img"
-		resValue.Err = true
-
-		res.WriteHeader(400)
-		fmt.Fprint(res, resValue)
-		return
-	}
-
-	resValue.ImgPath = imgName
-	resValue.Message = "success"
-	resValue.Err = false
-
-	resJson, _ := json.Marshal(resValue)
-
-	fmt.Fprint(res, string(resJson))
 }
