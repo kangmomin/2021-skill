e80974165e654fe90f832969db75e26783b8d209
koldin108902 <alex108902@naver.com>
2021-12-23 13:44:22 +0900

    get ref reply

--- router/reply.go
+++ router/reply.go
@@ -29,20 +29,37 @@ func GetReply(res http.ResponseWriter, req *http.Request) {
 
 	db := conn.DB
 
-	reply, err := db.Query("SELECT * FROM reply WHERE postId=? ORDER BY id DESC", postId)
+	reply, err := db.Query("SELECT * FROM reply WHERE postId=? AND refReplyId=0", postId)
 	if err != nil {
 		res.WriteHeader(http.StatusBadRequest)
 		fmt.Fprint(res, "error during get reply data")
 		return
 	}
-	var replies []structure.Reply
+
+	refReply, err := db.Query("SELECT * FROM reply WHERE postId=? AND refReplyId!=0 ORDER BY id", postId)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(res, "error during get ref_reply data")
+		return
+	}
+
+	var replies structure.GetReply
 
 	for reply.Next() {
 		var row structure.Reply
+		var refReplyId int
 
-		reply.Scan(&row.Id, &row.OwnerId, &row.Description, &row.PostId, &row.Time)
+		reply.Scan(&row.Id, &row.OwnerId, &row.Description, &row.PostId, &refReplyId, &row.Time)
 		row.Created = row.Time.Format("06-01-02 15:04")
-		replies = append(replies, row)
+		replies.Reply = append(replies.Reply, row)
+	}
+
+	for refReply.Next() {
+		var row structure.RefReply
+
+		refReply.Scan(&row.Id, &row.OwnerId, &row.Description, &row.PostId, &row.RefReplyId, &row.Time)
+		row.Created = row.Time.Format("06-01-02 15:04")
+		replies.RefReply = append(replies.RefReply, row)
 	}
 
 	resJson, _ := json.Marshal(replies)
