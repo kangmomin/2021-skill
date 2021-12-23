cb46c809e712c528059e4195896f149315d9c838
koldin108902 <alex108902@naver.com>
2021-12-23 13:44:42 +0900

    ref reply struct추가

--- structure/reply.go
+++ structure/reply.go
@@ -10,3 +10,18 @@ type Reply struct {
 	Time        time.Time `json:"time"`
 	Created     string    `json:"created"`
 }
+
+type RefReply struct {
+	Id          int       `json:"id"`
+	OwnerId     int       `json:"ownerId"`
+	Description string    `json:"description"`
+	PostId      int       `json:"postId"`
+	Time        time.Time `json:"time"`
+	Created     string    `json:"created"`
+	RefReplyId  int       `json:"refReplyId"`
+}
+
+type GetReply struct {
+	Reply    []Reply    `json:"reply"`
+	RefReply []RefReply `json:"refReply"`
+}
